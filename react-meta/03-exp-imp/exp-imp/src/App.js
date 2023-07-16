cc638d86f26bdc0bcb0cb40ef1b959978f1e94e9
Moin Ahamed <ahamedjust@gmail.com>
2023-07-16 12:26:39 +0530

    export import thing

---
+++ react-meta/03-exp-imp/exp-imp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Heading from './Heading';
+
+function App() {
+  return (
+    <div>
+      <Heading />; 
+    </div>
+  );
+}
+
+export default App;
