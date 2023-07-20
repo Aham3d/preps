41f7371d0082f05981b05417ad5fb6d879d2b7b2
Moin Ahamed <ahamedjust@gmail.com>
2023-07-20 22:49:10 +0530

    multiple-components with props

---
+++ react-meta/multiple-components/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>add three card elements</h1>
+        <Card h2="first card's h2" h3="first card's h3"/>;
+        <Card h2="second card's h2" h3="second card's h3"/>;
+        <Card h2="third card's h2" h3="third card's h3"/>;
+    </div>
+  );
+}
+
+export default App;
