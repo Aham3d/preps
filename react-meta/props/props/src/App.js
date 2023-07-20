864efd042c1083dab43117ef757f20f938aba4cf
Moin Ahamed <ahamedjust@gmail.com>
2023-07-20 22:48:49 +0530

    props

---
+++ react-meta/props/props/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Heading from './Heading';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading firstName="bob"/>
+      <Heading firstName="bobby" />
+    </div>
+  );
+}
+
+export default App;
