6fcace8de18233f3a553ac1974d0d6035dee8da2
Moin Ahamed <ahamedjust@gmail.com>
2023-08-18 12:26:54 +0530

    tweeks

--- react-meta/customizing/src/App.js
+++ react-meta/customizing/src/App.js
@@ -1,8 +1,10 @@
-// import Intro1 from "../components/intro1";
+import Intro1 from "./components/intro1";
+import Intro2 from "./components/intro2";
+import Nav from "./components/nav";
 function App() {
   return (
     <div>
-      <p>good</p>
+      < Nav />
     </div>
   )
 };
