30d4f59bd63b239f6dae33b0ff4f28012ce6acd6
Moin Ahamed <ahamedjust@gmail.com>
2023-08-23 22:43:48 +0530

    arranging files to start the second project - yesterday

---
+++ react-fcc/02-airbnb/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '@/components/Nav';
+
+const page = () => {
+  return (
+    <div>
+      <Nav />
+      <Body />
+    </div>
+  )
+}
+
+export default page; 
---
+++ react-fcc/customizing/src/App.js
@@ -0,0 +1,12 @@
+import Intro1 from "./components/intro1";
+import Intro2 from "./components/intro2";
+import Nav from "./components/nav";
+function App() {
+  return (
+    <div>
+      < Nav />
+    </div>
+  )
+};
+  
+export default App;
