643ed96118b45e68633e166709cdc73e1b5d0121
Moin Ahamed <ahamedjust@gmail.com>
2023-09-19 21:09:30 +0530

    revising

--- react-meta/customizing/src/App.js
+++ react-meta/customizing/src/App.js
@@ -1,12 +1,20 @@
 import Intro1 from "./components/intro1";
 import Intro2 from "./components/intro2";
 import Nav from "./components/nav";
+import Footer from "./components/footer";
+import Intro3 from "./components/intro3";
+
 function App() {
   return (
     <div>
       < Nav />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
     </div>
   )
 };
   
 export default App;
+// revision
