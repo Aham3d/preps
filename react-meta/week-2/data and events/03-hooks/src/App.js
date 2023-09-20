0859a1dc6fabaf6fa818688a4bd104ee2dd7c5af
Moin Ahamed <ahamedjust@gmail.com>
2023-09-20 17:41:38 +0530

    useState

--- react-meta/week-2/data and events/03-hooks/src/App.js
+++ react-meta/week-2/data and events/03-hooks/src/App.js
@@ -1,10 +1,23 @@
-// common event handling & syntax for handlers
-import Promo from './components/Promo';
 import './App.css';
+import {useState} from "react"
+
 function App() {
+
+  const [Text, setText] = useState('hello')
+
+  function handleChange(e) {
+    setText(e.target.value); 
+  }
+
   return (
-    <Promo />
-  );
+    <>
+      <input value={Text} onChange={handleChange}/>
+      <p>input says: {Text}</p>
+      <button onClick={() => setText('hi')}>
+        Reset
+      </button>
+    </>
+  )
 }
 
 export default App;
