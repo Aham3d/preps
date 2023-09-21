2ca814c6578298ad26424f21e3cf28fa9fa52114
Moin Ahamed <ahamedjust@gmail.com>
2023-09-21 11:10:16 +0530

    useReducer example

--- react-meta/customizing/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="copyright">
-      <p>Made by Me</p>
-    </div>
-  );
-};
-
-export default Footer; 
---
+++ react-meta/week-2/02-data and events/06-reducer/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useReducer } from 'react'
+
+const reducer = (state, action) => {
+  if(action.type === 'ride') return {money: state.money + 10}; 
+  if(action.type === 'fuel') return {money: state.money - 50}; 
+  return new Error(); 
+}
+
+function App() {
+
+  const initialState = {money: 100}; 
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <h1>Wallet: {state.money}</h1>
+      <div>
+        <button onClick={() => dispatch({type: 'ride'})}>
+          A new Customer
+        </button>
+        <button onClick={() => dispatch({type: 'fuel'})}>
+          Refill the tank
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
