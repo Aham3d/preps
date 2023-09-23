c8151cc396bfc0626846437153a3b03999a652cb
Moin Ahamed <ahamedjust@gmail.com>
2023-09-23 22:48:51 +0530

    willing to get started with the week 3 contents, but the react-router installation failing again and again. and can't -npm run dev - in the nextjs file. lost motivation after trying for a while

---
+++ react-meta/week-3/01-linking and routing/01-the-navbar/src/App.js
@@ -0,0 +1,11 @@
+// common event handling & syntax for handlers
+import './App.css';
+import Btn from './Btn';
+function App() {
+  return (
+    <Btn />
+  );
+}
+
+export default App;
+// revision
