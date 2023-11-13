a100af4ebc8fad302b5ea36f924b4df7210694cf
Moin Ahamed <ahamedjust@gmail.com>
2023-11-13 22:42:14 +0530

    created a folder for the extra projects.started with the react-todo tutorial project which is from web dev simplified, just to go through the concepts. done with the rendering static things required for the project and got stuck in while rendering todo list after clicking on ADD button. will go through that tomorrow

---
+++ react-extras/react-todo/src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [newItem, setNewItem] = useState("")
+
+  const [todos, setTodos] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault(); 
+
+    setTodos(currentTodos =>  {
+      return [
+        ...currentTodos, 
+        {id: crypto.randomUUID(), title: newItem, completed:false},
+      ]
+    })
+    setNewItem("")
+  }
+
+
+  return (
+    <>
+      <form className='new-item-form' onSubmit={handleSubmit}>
+        <div className='form-row'>
+          <label htmlFor='item'>
+            New item  
+          </label>
+          <input 
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            type='text'
+            id='item'
+          />
+        </div>
+        <button 
+          className='btn'>
+          Add
+        </button>
+      </form>
+      <h1 className='header'>
+        Todo List
+      </h1>
+      <ul className='list'>
+        {todos.map(todo => {
+          return ( 
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed}/>
+                {todo.title}
+              </label>
+              <button className='btn delete'>
+                delete
+              </button>
+            </li>
+          )
+        })}
+        
+      </ul>
+    </>
+  )
+}
+
+export default App
