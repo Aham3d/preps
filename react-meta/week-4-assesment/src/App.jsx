04e6e897333acf9320d7ffc7ba14dd5111c05977
Moin Ahamed <ahamedjust@gmail.com>
2023-11-28 14:20:17 +0530

    completed the project the understood it

--- react-meta/week-4-assesment/src/App.jsx
+++ react-meta/week-4-assesment/src/App.jsx
@@ -2,7 +2,39 @@ import { useState, useRef } from "react";
 
 function App() {
 
+  const inputRef = useRef(null);
+  // const resultRef = useRef(null);
   const [result , setResult] = useState(0); 
+
+  function plus(e) {
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value))
+  }
+
+  function minus(e) { 
+  	e.preventDefault(); 
+    setResult((result) => result - Number(inputRef.current.value)); 
+  };
+
+  function times(e) { 
+  	e.preventDefault(); 
+    setResult((result) => result * Number(inputRef.current.value)); 
+  }; 
+
+  function divide(e) { 
+    e.preventDefault(); 
+    setResult((result) => result / Number(inputRef.current.value)); 
+  };
+
+  function resetInput(e) { 
+    e.preventDefault();
+    inputRef.current.value = 0; 
+  }; 
+ 
+  function resetResult(e) { 
+    e.preventDefault();
+    setResult(0) 
+  }; 
 
   return (
     <div className="App">
@@ -17,13 +49,14 @@ function App() {
           type="number" 
           pattern="[0-9]"
           placeholder="type a number"
+          ref={inputRef}
         />
-        <button>+</button>
-        <button>-</button>
-        <button>*</button> 
-        <button>/</button>
-        <button>reset input</button>
-        <button>reset result</button>
+        <button onClick={plus}>+</button>
+        <button onClick={minus}>-</button>
+        <button onClick={times}>*</button> 
+        <button onClick={divide}>/</button>
+        <button onClick={resetInput}>reset input</button>
+        <button onClick={resetResult}>reset result</button>
       </form>
     </div>
   )
